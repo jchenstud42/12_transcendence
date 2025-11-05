fc8e1575a313f57daa3a58bb9417fab91646997d
mjameau <mjameau@student.42.fr>
2025-11-05 15:30:08 +0100

    Main.ts avec validation input + boutons fonctionnels

--- Front/srcs/ts/main.ts
+++ Front/srcs/ts/main.ts
@@ -1,5 +1,4 @@
-// import { validateTextInput, validatePassword, sanitizeInput, validateEmail } from "./utils/inputValidFront.js";
-// --------------------------------------
+import { validateTextInput, validatePassword, sanitizeInput, validateEmail } from "./utils/inputValidFront.js";
 
 const page = document.getElementById("page")!;
 const register_form = document.getElementById("register-form")! as HTMLFormElement | null;
@@ -32,84 +31,84 @@ login_button.addEventListener("click", () => {
 });
 
 
-// if (!register_form) {
-// 	console.warn("Register form not found");
-// }
-// else {
-// 	register_form.addEventListener("submit", async (e) => {
-// 		e.preventDefault();
+if (!register_form) {
+	console.warn("Register form not found");
+}
+else {
+	register_form.addEventListener("submit", async (e) => {
+		e.preventDefault();
 
-// 		const inUsername = document.getElementById("username") as HTMLInputElement | null;
-// 		const inEmail = document.getElementById("email") as HTMLInputElement | null;
-// 		const inPassword = document.getElementById("password") as HTMLInputElement | null;
+		const inUsername = document.getElementById("username") as HTMLInputElement | null;
+		const inEmail = document.getElementById("email") as HTMLInputElement | null;
+		const inPassword = document.getElementById("password") as HTMLInputElement | null;
 
-// 		if (!inUsername || !inEmail || !inPassword) {
-// 			console.error("Missing elements in the form");
-// 			return;
-// 		}
+		if (!inUsername || !inEmail || !inPassword) {
+			console.error("Missing elements in the form");
+			return;
+		}
 
-// 		const username = inUsername.value;
-// 		const email = inEmail.value;
-// 		const password = inPassword.value;
+		const username = inUsername.value;
+		const email = inEmail.value;
+		const password = inPassword.value;
 
-// 		const errors: string[] = [];
-// 		if (!validateEmail(email))
-// 			errors.push("Invalid email");
-// 		if (!validatePassword(password))
-// 			errors.push("Password must have 8 characters, one uppercase letter and one number");
-// 		if (!validateTextInput(username, 20))
-// 			errors.push("Invalid username");
+		const errors: string[] = [];
+		if (!validateEmail(email))
+			errors.push("Invalid email");
+		if (!validatePassword(password))
+			errors.push("Password must have 8 characters, one uppercase letter and one number");
+		if (!validateTextInput(username, 20))
+			errors.push("Invalid username");
 
-// 		if (errors.length > 0) {
-// 			alert("Errors:\n" + errors.join("\n"));
-// 			return;
-// 		}
+		if (errors.length > 0) {
+			alert("Errors:\n" + errors.join("\n"));
+			return;
+		}
 
-// 		const safeUsername = sanitizeInput(username);
-// 		const safeEmail = sanitizeInput(email);
+		const safeUsername = sanitizeInput(username);
+		const safeEmail = sanitizeInput(email);
 
-// 		const sendBack = {
-// 			username: safeUsername,
-// 			email: safeEmail,
-// 			password: password
-// 		};
+		const sendBack = {
+			username: safeUsername,
+			email: safeEmail,
+			password: password
+		};
 
-// 		const submit = register_form.querySelector('button[type="submit"]') as HTMLButtonElement | null;
-// 		if (submit) {
-// 			submit.disabled = true;
-// 			const originalTxt = submit.textContent;
-// 			submit.textContent = "Registering...";
-// 			try {
-// 				// ------------------------------------- A CHANGER ICI LE PATH TO REGISTER ------------------------------------------------------
-// 				const res = await fetch("??/register", {
-// 					// ------------------------------------------------------------------------------------------------------------------------------
-// 					method: "POST",
-// 					headers: {
-// 						"Content-Type": "application/json"
-// 					},
-// 					body: JSON.stringify(sendBack),
-// 				});
-// 				if (res.ok) {
-// 					alert("Registration successful! You can now log in.");
-// 					register_form.reset();
-// 				}
-// 				else {
-// 					const err = await res.json().catch(() => null);
-// 					alert("Server error: " + (err?.message || res.statusText));
-// 				}
-// 			}
-// 			catch (err) {
-// 				console.error("Fetch error:", err);
-// 				alert("Network error. Try again later.");
-// 			}
-// 			finally {
-// 				if (submit) {
-// 					submit.disabled = false;
-// 					submit.textContent = originalTxt ?? "Register";
-// 				}
-// 			}
-// 		}
+		const submit = register_form.querySelector('button[type="submit"]') as HTMLButtonElement | null;
+		if (submit) {
+			submit.disabled = true;
+			const originalTxt = submit.textContent;
+			submit.textContent = "Registering...";
+			try {
+				// ------------------------------------- A CHANGER ICI LE PATH TO REGISTER ------------------------------------------------------
+				const res = await fetch("??/register", {
+					// ------------------------------------------------------------------------------------------------------------------------------
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify(sendBack),
+				});
+				if (res.ok) {
+					alert("Registration successful! You can now log in.");
+					register_form.reset();
+				}
+				else {
+					const err = await res.json().catch(() => null);
+					alert("Server error: " + (err?.message || res.statusText));
+				}
+			}
+			catch (err) {
+				console.error("Fetch error:", err);
+				alert("Network error. Try again later.");
+			}
+			finally {
+				if (submit) {
+					submit.disabled = false;
+					submit.textContent = originalTxt ?? "Register";
+				}
+			}
+		}
 
 
-// 	})
-// }
+	})
+}
