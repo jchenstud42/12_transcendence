36804d94915ec2563f958bb6a753ea99739f1936
ropkyns <ropkyns@gmail.com>
2025-11-07 16:51:40 +0100

    correction buttons & forms + add pong game (not playable)

--- Front/srcs/ts/main.ts
+++ Front/srcs/ts/main.ts
@@ -1,10 +1,12 @@
 import { validateTextInput, validatePassword, sanitizeInput, validateEmail } from "./utils/inputValidFront.js";
-
 const page = document.getElementById("page")!;
 const register_form = document.getElementById("register-form")! as HTMLFormElement | null;
 const login_form = document.getElementById("login-form")! as HTMLFormElement | null;
 const register_button = document.getElementById("register-button")!;
 const login_button = document.getElementById("login-button")!;
+
+//affichage des formulaires lorsque l'on clique sur un des boutons avec synchronisation pour cacher l'autre formulaire si il etait deja affiche
+//et cacher le formulaire si on reclique sur le boutton a nouveau
 
 register_button.addEventListener("click", () => {
 	if (login_form && !login_form.classList.contains("hidden")) {
@@ -194,4 +196,60 @@ else {
 			}
 		});
 	}
+}
+
+
+// POOOONNNNNNNG
+const paddle_left = document.getElementById("left-paddle") as HTMLDivElement;
+const paddle_right = document.getElementById("right-paddle") as HTMLDivElement;
+const ball = document.getElementById("ball") as HTMLDivElement;
+
+const PONG_WIDTH = 600;
+const PONG_HEIGHT = 400;
+const PADDLE_WIDTH = 10;
+const PADDLE_HEIGHT = 80;
+const BALL_SIZE = 10;
+
+
+const pong_button 		= 	document.getElementById("pong-button")!;
+const qmatch_button 	= 	document.getElementById("quick-match-button")!;
+const tournament_button = 	document.getElementById("tournament-button")!;
+
+const enterPlayerNbr_text 	= 	document.getElementById("enterPlayerNbr-text")! as HTMLHeadingElement;
+const playerNbr_text 	= 	document.getElementById("playerNbr-text")! as HTMLHeadingElement;
+const playerIncr_button = 	document.getElementById("increasePlayer-button")!;
+const playerDecr_button = 	document.getElementById("decreasePlayer-button")!;
+const OK_button 		= 	document.getElementById("OK-button")!;
+const play_button 		= 	document.getElementById("play-button")!;
+
+const playerName_container	= 	document.getElementById("playerName-container")! as HTMLDivElement;
+const playerName_input		= 	document.getElementById("playerName-input")! as HTMLInputElement;
+const playersList 			= 	document.getElementById("players-list") as HTMLDivElement;
+const playerColors 			= 	["text-red-400", "text-blue-400", "text-green-400", "text-yellow-400"];
+
+class Player {
+  name: string = "";
+  paddle: HTMLDivElement | null = null;
+  point: number = 0;
+  gameWon: number = 0;
+  isAi: boolean = false;
+
+  constructor(name: string, isAi: boolean) {
+    this.name = name;
+  }
+}
+
+pong_button.addEventListener("click", () => {
+	pong_button.classList.add("hidden");
+	paddle_left.classList.remove("hidden")
+	paddle_right.classList.remove("hidden")
+	ball.classList.remove("hidden")
+});
+
+function pong() {
+	let p1 = new Player("Paul", false);
+	p1.paddle = paddle_left
+	let p2 = new Player("Allan", false);
+	p2.paddle = paddle_right
+
 }
