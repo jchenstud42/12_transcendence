df50989d41e0f331d04d650286d1862f211dbd04
mjameau <mjameau@student.42.fr>
2025-12-20 14:40:18 +0100

    Oubli clean, tjrs ajout Readme

--- Frontend/js/UI/UI_helpers.js
+++
@@ -1,9 +0,0 @@
-/*
-Function pour changer l'affichage des menus (afficher un et cacher les autres)
-*/
-export function toggleMenu(main, ...toHide) {
-    if (!main)
-        return;
-    toHide.forEach(menu => menu === null || menu === void 0 ? void 0 : menu.classList.add("hidden"));
-    main.classList.toggle("hidden");
-}
